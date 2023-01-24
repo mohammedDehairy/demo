f2858d572a33043c490aa1370560f2ff62d64161
mohamede <mohamed.eldoheiri@adyen.com>
2023-01-24 23:40:25 +0100

    commit

--- Sources/Platform SDK/Platform_SDK.swift
+++ Sources/Platform SDK/Platform_SDK.swift
@@ -1,9 +1,9 @@
-//import AdyenDropIn
+import AdyenDropIn
 
 public struct Platform_SDK {
     public private(set) var text = "Hello, World!"
 
     public init() {
-//        let config = DropInComponent.Configuration()
+        let config = DropInComponent.Configuration()
     }
 }
